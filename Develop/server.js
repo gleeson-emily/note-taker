da369009663ab3ea51f6404ab8c2c35aa34eea50
Emily Gleeson <engleeson@gmail.com>
2021-06-23 20:50:33 -0400

    setting up starter code, working on server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -16,6 +16,7 @@ const { notes } = require('./db/db.json')
 
 
 
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
